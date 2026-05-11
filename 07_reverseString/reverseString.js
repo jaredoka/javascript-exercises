358a641a5ebb249bca1648c641beb046b981bd20
jaredoka <66444307+jaredoka@users.noreply.github.com>
2026-05-11 16:55:45 +0800

    Add and complete exercise 7.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    return string.split('').reverse().join("");
+    };
 
-};
+//convert string into array then reverse the array.
 
 // Do not edit below this line
 module.exports = reverseString;
